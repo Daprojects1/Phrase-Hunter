4d608caabd6749852d2e88011f8f656dde81316b
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-03-24 19:25:54 +0000

    added life and also added logic to show selected items

--- helperFunctions/checkToMatch.js
+++ helperFunctions/checkToMatch.js
@@ -1,5 +1,21 @@
+let lives = document.querySelector(".livesNum")
+let life = 5 
+
 export function compareValues(winningArr, clickedArr) {
-    if (winningArr.length >= clickedArr.length) {
-        console.log(winningArr, clickedArr)
+    lives.innerHTML = `${life}`
+    let arr = []
+    if (life) {
+        winningArr.map(item => {
+            let c = item.children[0].innerHTML
+            arr.push(c)
+            if (clickedArr.includes(item.children[0].innerHTML)) {
+                item.children[0].classList.remove("main-span")
+                return;
+            }
+        })
     }
+    if (life === 0) {
+        document.querySelector(".mainBoard").innerHTML="Game completed"
+    }
+    console.log(arr)
 }
